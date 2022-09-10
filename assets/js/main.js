8c2ef664a3059ecab48dc3ad0f3e27677705b5ce
Carlos <carlosjr.ribas@gmail.com>
2022-09-10 11:37:11 -0300

    bc

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,26 @@
 
 const blocos = document.getElementsByClassName('bloco');
 
-const carousel = {
-    
+const fotos = () => {
+    const album = [];
+    let cont = 1;
+    while (cont < 12) {
+        album.push(`oi${cont}.jpeg`)
+        cont++;
+    }
+    return album;
 }
+const albumFotos = {
+    setPhotos : () => {
+        const container = document.getElementById('container-fotos')
+        fotos().forEach( foto => container.innerHTML+= albumFotos.divFoto({foto}) )
+    },
+    divFoto : ({foto}) => {
+        return `
+            <div class="photo" style="background-image:url(/assets/nosso-album/${foto});">
+
+            </div>
+        `
+    }
+}
+albumFotos.setPhotos()
